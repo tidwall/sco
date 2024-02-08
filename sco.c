134f63aea39c972f1d8e171f047c32c98f76ffa1
tidwall <joshbaker77@gmail.com>
2024-02-07 19:52:31 -0700

    Allow dladdr in FreeBSD

--- sco.c
+++ sco.c
@@ -1159,6 +1159,7 @@ const char *llco_method(void *caps) {
 
 #include <unwind.h>
 #include <string.h>
+#include <dlfcn.h>
 
 struct llco_dlinfo {
     const char      *dli_fname;     /* Pathname of shared object */
@@ -1166,7 +1167,10 @@ struct llco_dlinfo {
     const char      *dli_sname;     /* Name of nearest symbol */
     void            *dli_saddr;     /* Address of nearest symbol */
 };
+
+#ifdef __linux__
 int dladdr(const void *, void *);
+#endif
 
 static void llco_getsymbol(struct _Unwind_Context *uwc, 
     struct llco_symbol *sym)
@@ -1176,7 +1180,7 @@ static void llco_getsymbol(struct _Unwind_Context *uwc,
     int ip_before; /* unused */
     sym->ip = (void*)_Unwind_GetIPInfo(uwc, &ip_before);
     struct llco_dlinfo dlinfo = { 0 };
-    if (sym->ip && dladdr(sym->ip, &dlinfo)) {
+    if (sym->ip && dladdr(sym->ip, (void*)&dlinfo)) {
         sym->fname = dlinfo.dli_fname;
         sym->fbase = dlinfo.dli_fbase;
         sym->sname = dlinfo.dli_sname;
